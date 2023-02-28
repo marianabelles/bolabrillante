cf77a7a2f84a38695dc249465b5529b1fdcc6845
Mariana Lescano <mariany01@hotmail.com>
2023-02-28 14:37:45 -0300

    Primer escritura en el repositorio

---
+++ js/curriculum.js
@@ -0,0 +1,24 @@
+document.getElementById('boton1').onclick= function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo1").innerHTML= "hola@sitioincreible.mx "
+}
+document.getElementById('boton2').onclick= function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo2").innerHTML= " 12345678"
+}    
+document.getElementById('boton3').onclick= function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo3").innerHTML= "26 "
+}    
+document.getElementById('boton4').onclick= function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo4").innerHTML= "10/04/1996 "
+}    
+document.getElementById('boton5').onclick= function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo5").innerHTML= "El Carril "
+}    
+document.getElementById('boton6').onclick= function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo6").innerHTML= "Argentina"
+}    
